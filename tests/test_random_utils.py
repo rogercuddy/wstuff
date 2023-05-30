b013eea4f3f0afbe9971f5e99549fd5c76ef670e
rcuddy <roger.cuddy@gmail.com>
2023-05-29 22:32:46 -0400

    Added docstrings to test files and initd pylint.xml

--- tests/test_random_utils.py
+++ tests/test_random_utils.py
@@ -1,87 +1,137 @@
+""" Unit tests for the random_utils module. """
 import string
 
 import pytest
 
-import lib.random_utils as random_utils
+from lib import random_utils
 
 
 @pytest.mark.repeat(10)
 def test_random_string():
-    s = random_utils.random_string(10)
-    assert len(s) == 10
-    assert all(c in string.ascii_letters + string.digits for c in s)
+    """
+    Test the generation of a random string of a specified length.
+
+    This test checks that a generated string has the correct length and only contains alphanumeric characters.
+    """
+    t_val = random_utils.random_string(10)
+    assert len(t_val) == 10 and all(c in string.ascii_letters + string.digits for c in t_val)
 
 
 @pytest.mark.repeat(10)
 def test_random_string_with_punctuation():
-    s = random_utils.random_string(10, include_punctuation=True)
-    assert len(s) == 10
-    assert all(c in string.ascii_letters + string.digits + string.punctuation for c in s)
+    """
+    Test the generation of a random string with punctuation of a specified length.
+
+    This test checks that a generated string has the correct length and only contains alphanumeric characters
+    and punctuation.
+    """
+    t_val = random_utils.random_string(10, include_punctuation=True)
+    assert len(t_val) == 10 and all(c in string.ascii_letters + string.digits + string.punctuation for c in t_val)
 
 
 @pytest.mark.repeat(10)
 def test_random_lowercase_string():
-    s = random_utils.random_lowercase_string(10)
-    assert len(s) == 10
-    assert all(c in string.ascii_lowercase + string.digits for c in s)
+    """
+    Test the generation of a random lowercase string of a specified length.
+
+    This test checks that a generated string has the correct length and only contains lowercase alphanumeric characters.
+    """
+    t_val = random_utils.random_lowercase_string(10)
+    assert len(t_val) == 10 and all(c in string.ascii_lowercase + string.digits for c in t_val)
 
 
 @pytest.mark.repeat(10)
 def test_random_uppercase_string():
-    s = random_utils.random_uppercase_string(10)
-    assert len(s) == 10
-    assert all(c in string.ascii_uppercase + string.digits for c in s)
+    """
+    Test the generation of a random uppercase string of a specified length.
+
+    This test checks that a generated string has the correct length and only contains uppercase alphanumeric characters.
+    """
+    t_val = random_utils.random_uppercase_string(10)
+    assert len(t_val) == 10 and all(c in string.ascii_uppercase + string.digits for c in t_val)
 
 
 @pytest.mark.repeat(10)
 def test_random_digits_string():
-    s = random_utils.random_digits_string(10)
-    assert len(s) == 10
-    assert all(c in string.digits for c in s)
+    """
+    Test the generation of a random string of digits of a specified length.
+
+    This test checks that a generated string has the correct length and only contains digit characters.
+    """
+    t_val = random_utils.random_digits_string(10)
+    assert len(t_val) == 10 and all(c in string.digits for c in t_val)
 
 
 @pytest.mark.repeat(10)
 def test_random_punctuation_string():
-    s = random_utils.random_punctuation_string(10)
-    assert len(s) == 10
-    assert all(c in string.punctuation for c in s)
+    """
+    Test the generation of a random string of punctuation characters of a specified length.
+
+    This test checks that a generated string has the correct length and only contains punctuation characters.
+    """
+    t_val = random_utils.random_punctuation_string(10)
+    assert len(t_val) == 10 and all(c in string.punctuation for c in t_val)
 
 
 @pytest.mark.repeat(10)
 def test_random_letters_string():
-    s = random_utils.random_letters_string(10)
-    assert len(s) == 10
-    assert all(c in string.ascii_letters for c in s)
+    """
+    Test the generation of a random string of letters of a specified length.
+
+    This test checks that a generated string has the correct length and only contains letter characters.
+    """
+    t_val = random_utils.random_letters_string(10)
+    assert len(t_val) == 10 and all(c in string.ascii_letters for c in t_val)
 
 
 # Test that exceptions are raised for invalid input
 @pytest.mark.repeat(10)
 def test_invalid_length():
+    """
+    Test that an exception is raised when the length of the string to generate is invalid (i.e., zero or negative).
+    """
     with pytest.raises(AssertionError):
         random_utils.random_string(0)
 
 
 def test_no_charset():
+    """
+    Test that an exception is raised when no charset is selected for the generation of the random string.
+    """
+    # pylint: disable=protected-access
     with pytest.raises(AssertionError):
         random_utils._random_string(10, include_lowercase=False, include_uppercase=False, include_digits=False,
                                     include_punctuation=False)
 
 
 def test_random_string_min_length():
-    s = random_utils.random_string(1)
-    assert len(s) == 1
-    assert any(c in string.ascii_letters + string.digits for c in s)
+    """
+    Test the generation of a random string with a minimum length of 1.
+
+    This test checks that a generated string has the correct length and contains at least one alphanumeric character.
+    """
+    # pylint: disable=protected-access
+    t_val = random_utils.random_string(1)
+    assert len(t_val) == 1 and any(c in string.ascii_letters + string.digits for c in t_val)
 
 
 @pytest.mark.repeat(10)
 def test_random_lowercase_string_no_digits():
-    s = random_utils.random_lowercase_string(10, include_digits=False)
-    assert len(s) == 10
-    assert all(c in string.ascii_lowercase for c in s)
+    """
+    Test the generation of a random lowercase string of a specified length without digits.
+
+    This test checks that a generated string has the correct length and only contains lowercase letter characters.
+    """
+    t_val = random_utils.random_lowercase_string(10, include_digits=False)
+    assert len(t_val) == 10 and all(c in string.ascii_lowercase for c in t_val)
 
 
 @pytest.mark.repeat(10)
 def test_random_uppercase_string_no_digits():
-    s = random_utils.random_uppercase_string(10, include_digits=False)
-    assert len(s) == 10
-    assert all(c in string.ascii_uppercase for c in s)
+    """
+    Test the generation of a random uppercase string of a specified length without digits.
+
+    This test checks that a generated string has the correct length and only contains uppercase letter characters.
+    """
+    t_val = random_utils.random_uppercase_string(10, include_digits=False)
+    assert len(t_val) == 10 and all(c in string.ascii_uppercase for c in t_val)
--- tests/test_serialization_utils.py
+++ tests/test_serialization_utils.py
@@ -4,8 +4,17 @@ from lib.serialization_utils import JsonSerializer, YamlSerializer, PickleSerial
 
 
 class TestSimpleSerialization:
+    """
+    Test cases for simple serialization using JSON, YAML, and Pickle serializers.
+
+    This class contains test methods to check serialization and deserialization of dictionary objects with JSON, YAML,
+    and Pickle serializers. It includes tests for both non-empty and empty dictionaries.
+    """
 
     def test_json_serializer(self):
+        """
+        Test the JSON serializer with a non-empty dictionary.
+        """
         data_serializer = DataSerializer(JsonSerializer())
         data = {"key": "value"}
         serialized_data = data_serializer.serialize(data)
@@ -14,7 +23,9 @@ class TestSimpleSerialization:
         assert deserialized_data == data
 
     def test_json_serializer_empty_dict(self):
-        # Test with empty dictionary
+        """
+        Test the JSON serializer with an empty dictionary.
+        """
         data_serializer = DataSerializer(JsonSerializer())
         data = {}
         serialized_data = data_serializer.serialize(data)
@@ -23,6 +34,9 @@ class TestSimpleSerialization:
         assert deserialized_data == data
 
     def test_yaml_serializer(self):
+        """
+        Test the YAML serializer with a non-empty dictionary.
+        """
         data_serializer = DataSerializer(YamlSerializer())
         data = {"key": "value"}
         serialized_data = data_serializer.serialize(data)
@@ -31,8 +45,10 @@ class TestSimpleSerialization:
         assert deserialized_data == data
 
     def test_yaml_serializer_empty_dict(self):
+        """
+        Test the YAML serializer with an empty dictionary.
+        """
         data_serializer = DataSerializer(YamlSerializer())
-        # Test with empty dictionary
         data = {}
         serialized_data = data_serializer.serialize(data)
         assert serialized_data.strip() == '{}'
@@ -40,6 +56,9 @@ class TestSimpleSerialization:
         assert deserialized_data == data
 
     def test_pickle_serializer(self):
+        """
+        Test the Pickle serializer with a non-empty dictionary.
+        """
         data_serializer = DataSerializer(PickleSerializer())
         data = {"key": "value"}
         serialized_data = data_serializer.serialize(data)
@@ -47,8 +66,10 @@ class TestSimpleSerialization:
         assert deserialized_data == data
 
     def test_pickle_serializer_empty_dict(self):
+        """
+        Test the Pickle serializer with an empty dictionary.
+        """
         data_serializer = DataSerializer(PickleSerializer())
-        # Test with empty dictionary
         data = {}
         serialized_data = data_serializer.serialize(data)
         deserialized_data = data_serializer.deserialize(serialized_data)
@@ -56,31 +77,61 @@ class TestSimpleSerialization:
 
 
 class TestObjerialization:
+    """
+    Test cases for serialization of custom objects using JSON, YAML, and Pickle serializers.
+
+    This class contains test methods to check serialization and deserialization of custom objects with JSON, YAML,
+    and Pickle serializers.
+    """
+
     class CustomObject:
+        """
+        A simple custom object to be used in the serialization tests.
+        """
+        # pylint: disable=too-few-public-methods
+
         def __init__(self, name, value):
+            """
+            Initialize the custom object with a name and a value.
+            """
             self.name = name
             self.value = value
 
         def __eq__(self, other):
+            """
+            Define equality for the custom object.
+            """
             return self.__dict__ == other.__dict__
 
     @pytest.fixture
     def custom_object(self):
+        """
+        Pytest fixture for creating a custom object.
+        """
         return TestObjerialization.CustomObject("test_name", "test_value")
 
     def test_json_serializer(self, custom_object):
+        """
+        Test the JSON serializer with a custom object.
+        """
         data_serializer = DataSerializer(JsonSerializer())
         serialized_data = data_serializer.serialize(custom_object.__dict__)
         deserialized_data = data_serializer.deserialize(serialized_data)
         assert deserialized_data == custom_object.__dict__
 
     def test_yaml_serializer(self, custom_object):
+        """
+        Test the YAML serializer with a custom object.
+        """
         data_serializer = DataSerializer(YamlSerializer())
         serialized_data = data_serializer.serialize(custom_object.__dict__)
         deserialized_data = data_serializer.deserialize(serialized_data)
         assert deserialized_data == custom_object.__dict__
 
     def test_pickle_serializer(self, custom_object):
+        """
+        Test the Pickle serializer with a custom object.
+        """
         data_serializer = DataSerializer(PickleSerializer())
         serialized_data = data_serializer.serialize(custom_object)
         deserialized_data = data_serializer.deserialize(serialized_data)
@@ -88,36 +139,68 @@ class TestObjerialization:
 
 
 class TestSerializerStrReprMethods:
+    """
+    Test cases for the __str__ and __repr__ methods of
+        JsonSerializer, YamlSerializer, PickleSerializer, and DataSerializer.
+
+    This class contains test methods to check the outputs of __str__ and __repr__ methods of JsonSerializer,
+    YamlSerializer, PickleSerializer, and DataSerializer.
+    """
+
     def test_json_serializer_str(self):
+        """
+        Test the __str__ method of JsonSerializer.
+        """
         json_serializer = JsonSerializer()
         assert str(json_serializer) == 'JsonSerializer()'
 
     def test_json_serializer_repr(self):
+        """
+        Test the __repr__ method of JsonSerializer.
+        """
         json_serializer = JsonSerializer()
         assert repr(json_serializer) == 'JsonSerializer()'
 
     def test_yaml_serializer_str(self):
+        """
+        Test the __str__ method of YamlSerializer.
+        """
         yaml_serializer = YamlSerializer()
         assert str(yaml_serializer) == 'YamlSerializer()'
 
     def test_yaml_serializer_repr(self):
+        """
+        Test the __repr__ method of YamlSerializer.
+        """
         yaml_serializer = YamlSerializer()
         assert repr(yaml_serializer) == 'YamlSerializer()'
 
     def test_pickle_serializer_str(self):
+        """
+        Test the __str__ method of PickleSerializer.
+        """
         pickle_serializer = PickleSerializer()
         assert str(pickle_serializer) == 'PickleSerializer()'
 
     def test_pickle_serializer_repr(self):
+        """
+        Test the __repr__ method of PickleSerializer.
+        """
         pickle_serializer = PickleSerializer()
         assert repr(pickle_serializer) == 'PickleSerializer()'
 
     def test_data_serializer_str(self):
+        """
+        Test the __str__ method of DataSerializer.
+        """
         json_serializer = JsonSerializer()
         data_serializer = DataSerializer(json_serializer)
         assert str(data_serializer) == 'DataSerializer(serializing_strategy=JsonSerializer)'
 
     def test_data_serializer_repr(self):
+        """
+        Test the __repr__ method of DataSerializer.
+        """
         json_serializer = JsonSerializer()
         data_serializer = DataSerializer(json_serializer)
         assert repr(data_serializer) == 'DataSerializer(serializing_strategy=JsonSerializer)'
